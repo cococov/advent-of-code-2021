d6590fcbcbe3ec0b66ece2f15ec8dc4f9458ba1b
cococov <juank.yea@hotmail.com>
2021-12-01 23:40:45 -0300

    fix: delete unnecessary reducers

--- day_1/day_1.ts
+++ day_1/day_1.ts
@@ -4,12 +4,10 @@ const result1: number = input.reduce((acc, curr, index, arr) => (index !== 0 && 
 
 console.log(`Answer 1: ${result1}`);
 
-const sum = (a: number, b: number) => a + b;
-
 const result2: number = input.reduce((acc, _curr, index, arr) => {
   return (index < 3)
     ? acc
-    : arr.slice(index - 3, index).reduce(sum) < arr.slice(index - 2, index + 1).reduce(sum)
+    : arr[index - 3] < arr[index]
       ? ++acc
       : acc;
 }, 0);
