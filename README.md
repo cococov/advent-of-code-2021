69a218f34d95522f8f14aeecab58052f5760d24c
cococov <juank.yea@hotmail.com>
2021-12-05 01:30:34 -0300

    feat: day 4

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Solutions for [Advent of Code 2021](https://adventofcode.com/2021).
 - [Day 1 | TypeScript](./day_1)
 - [Day 2 | TypeScript](./day_2)
 - [Day 3 | TypeScript](./day_3)
+- [Day 4 | Ruby](./day_4)
---
+++ day_4/day_4.rb
@@ -0,0 +1,64 @@
+input = File.open('input', 'r').read.split("\n\n")
+numbers = input.shift.split(/,/).map(&:to_i)
+boards = input.map { |board| board.split(/\n/).map{ |row| row.strip.split(/\s+/).map(&:to_i) } }
+
+def set_mark state, number
+  state.map do |board|
+    board.map do |row|
+      row.map { |cell| cell == number ? 'X' : cell }
+    end
+  end
+end
+
+def someone_win? state
+  state.each do |board|
+    board.each do |row|
+      return board if row.uniq === ['X']
+    end
+    board.transpose.each do |row|
+      return board if row.uniq === ['X']
+    end
+  end
+  false
+end
+
+def get_board_score board, winner_number
+  board.flatten.map { |i| i == 'X' ? 0 : i }.sum * winner_number
+end
+
+def bingo_subsystem numbers, boards
+  mut_state = boards.dup
+
+  numbers.each do |number|
+    mut_state = set_mark(mut_state, number)
+    winner_board = someone_win?(mut_state)
+    return { winner_board: winner_board, winner_number: number } if winner_board
+  end
+end
+
+def bingo_subsystem_for_loose numbers, boards
+  mut_state = boards.dup
+
+  numbers.each_with_index do |number, index|
+    mut_state = set_mark(mut_state, number)
+    winner_board = someone_win?(mut_state)
+    while winner_board do
+      return { winner_board: winner_board, winner_number: number } if mut_state.size == 1
+      mut_state.delete(winner_board) if winner_board
+      winner_board = someone_win?(mut_state)
+    end
+  end
+end
+
+def result_1 numbers, boards
+  result = bingo_subsystem(numbers, boards)
+  get_board_score(result[:winner_board], result[:winner_number])
+end
+
+def result_2 numbers, boards
+  result = bingo_subsystem_for_loose(numbers, boards)
+  get_board_score(result[:winner_board], result[:winner_number])
+end
+
+puts "Answer 2: #{result_1(numbers, boards)}"
+puts "Answer 2: #{result_2(numbers, boards)}"
