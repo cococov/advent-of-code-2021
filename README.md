0dc572d74b97fe551aa0d1f63f8ace5681b47857
cococov <juank.yea@hotmail.com>
2021-12-04 21:02:34 -0300

    feat: day 2

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Solutions for [Advent of Code 2021](https://adventofcode.com/2021).
 
 - [Day 1 | TypeScript](./day_1/README.md)
+- [Day 2 | TypeScript](./day_2/README.md)
--- day_2/day_2.ts
+++ day_2/day_2.ts
@@ -1,9 +1,54 @@
-const input: [string, number][] = Deno
+const input: ['forward' | 'down' | 'up', number][] = Deno
   .readTextFileSync('./input')
   .split('\n')
   .map(n => n.split(/\s/))
-  .map(([a, b]: string[]) => [a, Number(b)]);
+  .map(([a, b]: string[]) => [a as 'forward' | 'down' | 'up', Number(b)]);
 
-console.log(input);
+const result1: number = input
+  .reduce<[number, number]>(
+    (prev, curr): [number, number] => {
+      switch (curr[0]) {
+        case 'up':
+          return [prev[0], prev[1] - curr[1]];
+        case 'down':
+          return [prev[0], prev[1] + curr[1]];
+        case 'forward':
+          return [prev[0] + curr[1], prev[1]];
+        default:
+          return prev;
+      }
+    }, [0, 0])
+  .reduce((a, b) => a * b);
 
-export {};
+console.log(`Answer 1: ${result1}`);
+
+type position = {
+  cords: [number, number];
+  aim: number;
+}
+
+const result2: number = input
+  .reduce<position>(
+    (prev, curr): position => {
+      switch (curr[0]) {
+        case 'up':
+          return { ...prev, aim: prev['aim'] - curr[1] };
+        case 'down':
+          return { ...prev, aim: prev['aim'] + curr[1] };
+        case 'forward':
+          return {
+            ...prev,
+            cords: [
+              prev['cords'][0] + curr[1],
+              prev['cords'][1] + (prev['aim'] * curr[1])
+            ]
+          };
+        default:
+          return prev;
+      }
+    }, { cords: [0, 0], aim: 0 })['cords']
+  .reduce((a, b) => a * b);
+
+console.log(`Answer 2: ${result2}`);
+
+export { };
